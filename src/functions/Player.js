5d3105820118fe851f37d06655b68212c97affc8
Philip <philipcho101@hotmail.com>
2022-07-12 23:10:26 -0700

    Add Player factory function

---
+++ src/functions/Player.js
@@ -0,0 +1,27 @@
+const Player = () => {
+  const shots = Array.from(Array(100).keys());
+  const convert2Dto1D = (row, col) => row * 10 + col;
+  const convert1Dto2D = (val) => [Math.floor(val / 10), val % 10];
+  const attack = (board, row, col) => {
+    const val = convert2Dto1D(row, col);
+    if (!shots.includes(val)) return false;
+    board.receiveAttack(row, col);
+    const index = shots.indexOf(val);
+    shots.splice(index, 1);
+    return true;
+  };
+  const randAttack = (board) => {
+    if (shots.length === 0) return false;
+    const randIndex = Math.floor(Math.random() * shots.length);
+    const val = shots[randIndex];
+    const convertedVal = convert1Dto2D(val);
+    const row = convertedVal[0];
+    const col = convertedVal[1];
+    return attack(board, row, col);
+  };
+  return {
+    attack, randAttack,
+  };
+};
+
+module.exports = Player;
---
+++ src/tests/Player.test.js
@@ -0,0 +1,64 @@
+const Player = require('../functions/Player');
+const Gameboard = require('../functions/Gameboard');
+
+let player;
+let enemy;
+let playerBoard;
+let enemyBoard;
+
+let testplayerBoard;
+let testenemyBoard;
+
+beforeEach(() => {
+  player = Player();
+  enemy = Player();
+  playerBoard = Gameboard();
+  enemyBoard = Gameboard();
+
+  testplayerBoard = Array(10);
+  testenemyBoard = Array(10);
+  for (let i = 0; i < 10; i++) {
+    testplayerBoard[i] = Array(10).fill(0);
+    testenemyBoard[i] = Array(10).fill(0);
+  }
+});
+
+test('player attacks enemy board', () => {
+  expect(player.attack(enemyBoard, 0, 0)).toEqual(true);
+});
+
+test('player attacks enemy board, enemy board shows shot', () => {
+  player.attack(enemyBoard, 0, 0);
+  testplayerBoard[0][0] = 2;
+  expect(enemyBoard.getBoard()).toEqual(testplayerBoard);
+});
+
+test('player attacks enemy, enemy returns fire', () => {
+  player.attack(enemyBoard, 0, 0);
+  testplayerBoard[0][0] = 2;
+  enemy.attack(playerBoard, 4, 3);
+  testenemyBoard[4][3] = 2;
+  expect(playerBoard.getBoard()).toEqual(testenemyBoard);
+});
+
+test('player attacks enemy board at same location twice', () => {
+  player.attack(enemyBoard, 0, 0);
+  expect(player.attack(enemyBoard, 0, 0)).toEqual(false);
+});
+
+test('run rand attack 100 times', () => {
+  for (let i = 0; i < 100; i++) {
+    player.randAttack(enemyBoard);
+  }
+  for (let i = 0; i < 10; i++) {
+    testenemyBoard[i] = Array(10).fill(2);
+  }
+  expect(enemyBoard.getBoard()).toEqual(testenemyBoard);
+});
+
+test('run rand attack over 100 times', () => {
+  for (let i = 0; i < 100; i++) {
+    player.randAttack(enemyBoard);
+  }
+  expect(player.randAttack(enemyBoard)).toBe(false);
+});
