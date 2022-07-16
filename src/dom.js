5f0d77ec89284a61c507a6050e2c825ea1fc8611
Philip <philipcho101@hotmail.com>
2022-07-15 21:36:09 -0700

    Add user input and update board display

--- src/dom.js
+++ src/dom.js
@@ -1,31 +1,50 @@
-const dom = () => {
-  const createGrid = (board) => {
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[0].length; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.textContent = board[i][j];
-        grid.appendChild(cell);
-      }
+const createGrid = (board) => {
+  const grid = document.createElement('div');
+  grid.classList.add('grid');
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.textContent = '-';
+      grid.appendChild(cell);
     }
-    return grid;
-  };
-  const render = (playerBoard, enemyBoard) => {
-    const body = document.querySelector('body');
-    const playerTitle = document.createElement('div');
-    const enemyTitle = document.createElement('div');
-    playerTitle.textContent = 'Player';
-    enemyTitle.textContent = 'Enemy';
-    body.appendChild(playerTitle);
-    body.appendChild(createGrid(playerBoard));
-    body.appendChild(enemyTitle);
-    body.appendChild(createGrid(enemyBoard));
-  };
-  return {
-    render,
-  };
+  }
+  return grid;
 };
 
-module.exports = dom;
+const render = (playerBoard, enemyBoard) => {
+  const body = document.querySelector('body');
+  const playerTitle = document.createElement('div');
+  const enemyTitle = document.createElement('div');
+  const playerGrid = createGrid(playerBoard);
+  const enemyGrid = createGrid(enemyBoard);
+  playerTitle.textContent = 'Player';
+  enemyTitle.textContent = 'Enemy';
+  playerGrid.classList.add('player-grid');
+  enemyGrid.classList.add('enemy-grid');
+  body.appendChild(playerTitle);
+  body.appendChild(playerGrid);
+  body.appendChild(enemyTitle);
+  body.appendChild(enemyGrid);
+};
+
+const update = (board, isFirstPlayer) => {
+  const grid = isFirstPlayer ? document.querySelector('.player-grid') : document.querySelector('.enemy-grid');
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] > 1) {
+        if (board[i][j] === 3) {
+          grid.children[i * 10 + j].classList.add('hit');
+        } else {
+          grid.children[i * 10 + j].classList.add('miss');
+        }
+      }
+    }
+  }
+};
+
+export {
+  createGrid,
+  render,
+  update,
+};
--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 import './style.css';
+import * as dom from './dom';
 
 const Ship = require('./functions/Ship');
 const Player = require('./functions/Player');
 const Gameboard = require('./functions/Gameboard');
 
-const dom = require('./dom');
-
-const game = (() => {
+const game = () => {
   const player1 = Player();
   const playerboard = Gameboard();
 
   const player2 = Player();
   const enemyboard = Gameboard();
 
-  const display = dom();
+  dom.render(playerboard.getBoard(), enemyboard.getBoard());
 
-  display.render(playerboard.getBoard(), enemyboard.getBoard());
-})();
+  const sendAttack = (e) => {
+    const cell = e.target;
+    const parent = e.target.parentElement;
+    const index = [...parent.children].indexOf(cell);
+    const row = Math.floor(index / 10);
+    const col = index % 10;
+
+    if (parent.classList.contains('player-grid')) {
+      player2.attack(playerboard, row, col);
+      dom.update(playerboard.getBoard(), 1);
+    } else {
+      player1.attack(enemyboard, row, col);
+      dom.update(enemyboard.getBoard(), 0);
+    }
+  };
+  const addBoardListener = () => {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+      cell.addEventListener('click', sendAttack);
+    });
+  };
+  addBoardListener();
+};
+
+game();
